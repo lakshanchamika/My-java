a181bf1b34d610b898b9f57e39a0babea63b5b08
Chamika Lakshan <lakshanchamika956@gmail.com>
2025-08-26 19:35:14 +0530

    upgrate sum code

---
+++ program02.java
@@ -0,0 +1,30 @@
+public class program02 
+{
+    public static void main(String[] args)
+    {
+        //integer
+         int num1 = 10;
+         int num2 = 20;
+
+         num1=num1+num2;
+
+
+         System.out.print("output 01 is:- ");
+         System.out.println (num1);
+
+         //Float Type 01
+         double num3;
+
+         num3 = 3.15;
+         System.out.print("output 02 is:- ");
+         System.out.println(num3);
+
+         num3 = 2.51;
+
+         System.out.print("output 03 is:- ");
+         System.out.println(num3);
+
+
+    }
+
+}
