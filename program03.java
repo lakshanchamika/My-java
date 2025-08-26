49c189d4d7ce77455567ead9b0427cb022bc66dc
Chamika Lakshan <lakshanchamika956@gmail.com>
2025-08-26 20:36:34 +0530

    create 3rd program

--- program03.java
+++ program03.java
@@ -1,3 +1,11 @@
-public class program03 {
-    
+import java.util.Scanner;
+public class program03 
+{
+    public static void main(String[] arge)
+    {
+        System.out.print("Input Text :- ");
+        Scanner sun = new Scanner(System.in);
+
+        System.out.println(sun.next());
+    }
 }
