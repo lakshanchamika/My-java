f86dc4e8960cb0787818472d7434a0f05785973a
Chamika Lakshan <lakshanchamika956@gmail.com>
2025-08-26 20:47:57 +0530

    create new program and update old program

--- program04.java
+++ program04.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class program03 
+public class program04 
 {
     public static void main(String[] arge)
     {
@@ -8,6 +8,5 @@ public class program03
 
         
         System.out.println(sun.nextLine());
-        System.out.println(sun.next());
     }
 }
