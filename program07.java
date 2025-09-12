637ff6e8ed8b3c94de5ee347db7678ba0ed6b639
Chamika Lakshan <lakshanchamika956@gmail.com>
2025-09-12 20:14:24 +0530

    create new code and update sum error

--- program07.java
+++ program07.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 public class program07 {
 
     public static void main(String[] args){
+        System.out.println("Enter your numbrer:");
         Scanner A = new Scanner(System.in);
         int day = A.nextInt();
+
         A.close();
+
         if(day>0 && day<8){
             switch(day){
                 case 1:
@@ -17,7 +20,7 @@ public class program07 {
                     System.out.println("Today is Wednesday");
                     break;
                 case 4:
-                    System.out.println("Today is Thuesday");
+                    System.out.println("Today is Thursday");
                     break;
                 case 5:
                     System.out.println("Today is Friday");
