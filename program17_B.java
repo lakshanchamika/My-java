20e8d3159722ee82755fc820c8ef2c867a34b331
Chamika Lakshan <lakshanchamika956@gmail.com>
2025-09-17 20:27:38 +0530

    change 17 program

--- program17_B.java
+++ program17_B.java
@@ -1,4 +1,8 @@
 public class program17_B extends program17_A {
+
+    void internet(){
+        System.out.println("Browse Internet");
+    }
     public static void main(String[] args) {
         program17_B obj1 =new program17_B();
         obj1.call();
---
+++ program17_C.java
@@ -0,0 +1,14 @@
+public class program17_C extends program17_B {
+    void cam(){
+        System.out.println("Tis one have camara");
+
+    
+    }
+
+    public static void main(String[] args) {
+        program17_C obj2 = new program17_C();
+        obj2.call();
+        obj2.internet();
+        obj2.cam();
+    }
+}
